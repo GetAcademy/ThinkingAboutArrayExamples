6dc96c2c0e90b88d05e4fb79113967e6a851950d
Terje Kolderup <terje@kolderup.net>
2018-09-12 11:12:41 +0200

    x

--- ArrayEksempel1/Program.cs
+++ ArrayEksempel1/Program.cs
@@ -10,6 +10,43 @@ namespace ArrayEksempel1
     {
         static void Main(string[] args)
         {
+            Search2(args);
+        }
+
+        static void Search2(string[] args)
+        {
+            int[] intArray = { 10, 20, 30 };
+            int targetValue = Convert.ToInt32(args[0]);
+            int targetPos = 0;
+            while (targetPos < intArray.Length
+                && intArray[targetPos] != targetValue)
+            {
+                targetPos++;
+            }
+
+            var isFound = targetPos < intArray.Length;
+            var text = isFound
+                ? $"Fant verdien {targetValue} i indeks {targetPos}."
+                : $"Fant ikke verdien {targetValue}.";
+            Console.WriteLine(text);
+        }
+
+        static void Search(string[] args)
+        {
+            int[] intArray = { 10, 20, 30 };
+            int targetValue = Convert.ToInt32(args[0]);
+            int targetPos = 0;
+            while (targetPos < intArray.Length
+                && intArray[targetPos] != targetValue)
+            {
+                targetPos++;
+            }
+
+            var isFound = targetPos < intArray.Length;
+            var text = isFound
+                ? $"Fant verdien {targetValue} i indeks {targetPos}."
+                : $"Fant ikke verdien {targetValue}.";
+            Console.WriteLine(text);
         }
     }
 }
