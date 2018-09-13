de5964e26f81913d3530a8c9ef7841109a56f354
Terje Kolderup <terje@kolderup.net>
2018-09-13 09:49:09 +0200

    x

--- ArrayEksempel1/Program.cs
+++ ArrayEksempel1/Program.cs
@@ -10,9 +10,74 @@ namespace ArrayEksempel1
     {
         static void Main(string[] args)
         {
+            FindMode();
+            //Average();
+            //SortDemo();
             //args = new[] {"10", "11", "12"};
-            FindMax(args);
+            //FindMax(args);
             //Search(args);
+        }
+
+        private static void FindMode()
+        {
+            int[] surveyData = { 4, 7, 3, 8, 9, 7, 3, 9, 9, 3, 3, 10 };
+            Array.Sort(surveyData);
+            // {4, 7, 7, 9, 9, 9, 8, 3, 3, 3, 3, 10};
+            int mostFrequent = -1;
+            int highestFrequency = 0;
+            int currentFrequency = 0;
+            var arraySize = surveyData.Length;
+            for (int i = 0; i < arraySize; i++)
+            {
+                currentFrequency++;
+                if (i == arraySize - 1 || surveyData[i] != surveyData[i + 1])
+                {
+                    if (currentFrequency > highestFrequency)
+                    {
+                        highestFrequency = currentFrequency;
+                        mostFrequent = surveyData[i];
+                    }
+                    currentFrequency = 0;
+                }
+            }
+
+            Console.WriteLine($"Det var flest {mostFrequent}-ere ({highestFrequency}).");
+        }
+
+        private static void Average()
+        {
+            int[] gradeArray = { 87, 76, 100, 97, 64, 83, 88, 92, 74, 95 };
+            double sum = 0;
+            for (int i = 0; i < gradeArray.Length; i++)
+            {
+                sum += gradeArray[i];
+            }
+            double average = sum / gradeArray.Length;
+            Console.WriteLine(average);
+        }
+
+        private static void SortDemo()
+        {
+            string[] txts = new[] { "Hei", "heio", "på", "deg", "hurramegrundt" };
+            Console.WriteLine("Før sortering:");
+            foreach (var txt in txts)
+            {
+                Console.WriteLine(txt);
+            }
+            Array.Sort(txts, Compare);
+            Console.WriteLine("\nEtter sortering:");
+            foreach (var txt in txts)
+            {
+                Console.WriteLine(txt);
+            }
+
+        }
+
+        private static int Compare(string x, string y)
+        {
+            if (x.Length == y.Length) return x.CompareTo(y);
+            if (x.Length > y.Length) return -1;
+            return 1;
         }
 
         static void FindMax(string[] args)
